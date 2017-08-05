6359d27c52b9a89c526e033cdccd62b69578887a
YashaRavindra <30417548+YashaRavindra@users.noreply.github.com>
2017-08-05 14:33:21 -0400

    Add files via upload

---
+++ ProcessCameraFeed.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Camera
+{
+    public partial class ProcessCameraFeed : Form, Affdex.ImageListener
+    {
+        public ProcessCameraFeed(Affdex.Detector detector)
+        {
+            detector.setImageListener(this);
+            InitializeComponent();
+        }
+        public void onImageResults(Dictionary<int, Affdex.Face> faces, Affdex.Frame frame)
+        {
+            foreach (KeyValuePair<int, Affdex.Face> pair in faces)
+            {
+                Affdex.Face face = pair.Value;
+                if (face != null)
+                {
+                    foreach (PropertyInfo prop in typeof(Affdex.Emotions).GetProperties())
+                    {
+                        float value = (float)prop.GetValue(face.Emotions, null);
+                        string output = String.Format("{0}:{1:0.00}", prop.Name, value);
+                        System.Console.WriteLine(output);
+                    }
+                }
+            }
+            frame.Dispose();
+        }
+        
+        public void onImageCapture(Affdex.Frame frame)
+        {
+            frame.Dispose();
+
+        }
+
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Affdex.Detector detector = new Affdex.CameraDetector(0, 30, 30, 3, Affdex.FaceDetectorMode.LARGE_FACES);
+            ProcessCameraFeed feed = new ProcessCameraFeed(detector);
+            detector.setClassifierPath("C:\\Program Files\\Affectiva\\AffdexSDK\\data");
+            detector.setDetectAllEmotions(true);
+            detector.setDetectAllEmojis(true);
+            detector.setDetectSmirk(true);
+            detector.setDetectGender(true);
+            detector.setDetectGlasses(true);
+            detector.start();
+            feed.ShowDialog();
+            detector.stop();
+
+        }
+    }
+}
